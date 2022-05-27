63ea595c156468d931526b33d4fd77a7243b57e7
Vlad Mansurov <m_v2709@mail.ru>
2022-05-27 11:55:00 +0300

    task 2 completed

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let title = '';
-let screens = '';
-let screenPrice = '';
-let rollback = '';
-let fullPrice = '';
-let adaptive = '';
+let title = "Учебный проект Glo_JS";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 30000;
+let rollback = 27;
+let fullPrice = 50000;
+let adaptive = true;
 
-alert('Урок 1');
-
-console.log('Урок 1 выполнен');
+console.log(typeof (title));
+console.log(typeof (fullPrice));
+console.log(typeof (adaptive));
+console.log("Длина строки в переменно screens:", screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей / долларов / гривен / юани ");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей / долларов / гривен / юани ");
+console.log(screens.toLowerCase().split(" "));
+console.log("Процент отката:", fullPrice * (rollback / 100));
